2fd010074c432a00bca5c51c14917229365a1d18
Neodmit <52948791+Neodmit@users.noreply.github.com>
2021-06-21 01:13:02 +0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String args[]) throws InterruptedException, IOException {
         HashMap<String, List<String>> dataFromFiles = new HashMap<>();
 
-        for (String fileName : args[0].split(";")) {
+        for (String fileName : args[0].split(";")) {//src/main/resources/inputs/input1.csv;src/main/resources/inputs/input2.csv
             Thread thread = new Thread(new ReadingDataFromCSV(fileName, dataFromFiles));
             thread.start();
             thread.join();
